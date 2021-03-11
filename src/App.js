ee295a52672c70ba84bdee0dc7af43c94c5ffb99
augustas3000 <augusto.pasaulis@gmail.com>
2021-03-11 22:01:13 +0200

    test commit in different branch

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import HomePage from "./pages/homepage/homepage.component.jsx";
-
+//this is just to check if i still know how branching works.
 function App() {
   return <HomePage />;
 }
