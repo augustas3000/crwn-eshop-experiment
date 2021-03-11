332da5b873fe1744fc70270d613e5328e1f3d02a
augustas3000 <augusto.pasaulis@gmail.com>
2021-03-11 22:01:13 +0200

    global font added

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import HomePage from "./pages/homepage/homepage.component.jsx";
 import "./App.scss";
 
+//this is just to check if i still know how branching works.
 function App() {
   return <HomePage />;
 }
