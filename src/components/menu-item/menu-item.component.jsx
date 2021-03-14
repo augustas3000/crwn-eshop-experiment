ff81f9b265f6fecf4532c9ea259b99bf14651139
augustas3000 <augusto.pasaulis@gmail.com>
2021-03-14 10:15:37 +0200

    directory menu refactored to use material-ui grid and paper

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,27 +1,91 @@
 import React from "react";
-import "./menu-item.styles.scss";
+import { Grid, makeStyles, Paper } from "@material-ui/core";
 
-const MenuItem = ({ title, imageUrl, size }) => (
 
-  <div className={`${size} menu-item`}>
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
 
-  
-    <div
-      className="background-image"
-      style={{
-        backgroundImage: `url(${imageUrl})`,
-      }}
-    />
+  menuItem: {
+    height: ({size}) => size === "large" ? 360 : 240,
+    borderRadius: 0,
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    border: "1px solid black",
+    boxShadow: 'none',
+    overflow: 'hidden',
 
-    <div className="content">
-      <h1 className="title">{title.toUpperCase()}</h1>
-      <span className="subtitle">SHOP NOW</span>
-    </div>
-  </div>
-);
+    "& .backgroundImageClass": {
+      backgroundImage: ({imageUrl}) => `url(${imageUrl})`,
+      width: '100%',
+      height: '100%',
+      backgroundPosition: 'center',
+      backgroundSize: 'cover',
+    },
 
-function capitalizeFirstLetter(string) {
-  return string.charAt(0).toUpperCase() + string.slice(1);
-}
+
+    "&:hover": {
+      cursor: 'pointer',
+    
+      "& .backgroundImageClass": {
+        transform: 'scale(1.1)',
+        transition: 'transform 6s cubic-bezier(0.25, 0.45, 0.45, 0.95)'
+      },
+    
+      "& .content": {
+        opacity: 0.9
+      }
+    },
+
+    "& .content": {
+      height: 90,
+      padding: "0 25px",
+      display: "flex",
+      flexDirection: "column",
+      alignItems: "center",
+      justifyContent: "center",
+      border: "1px solid black",
+      backgroundColor: 'white',
+      opacity: 0.7,
+      position: 'absolute',
+
+      "& .title": {
+        fontWeight: "bold",
+        marginBottom: 6,
+        fontSize: 22,
+        color: "#4a4a4a",
+      },
+
+      "& .subtitle": {
+        fontWeight: "lighter",
+        fontSize: 16,
+      },
+    }
+  },
+}));
+
+
+const MenuItem = ({ title, imageUrl, size }) => {
+  const classes = useStyles({ size, imageUrl });
+
+  return (
+    <Grid item xs={12} sm={size === "large" ? 6 : 4}>
+      <Paper className={classes.menuItem}>
+
+        <div
+          className="backgroundImageClass"
+        />
+
+        <div className="content">
+          <h1 className="title">{title}</h1>
+          <span className="subtitle">SHOP NOW</span>
+        </div>
+      </Paper>
+    </Grid>
+  );
+};
+
 
 export default MenuItem;
