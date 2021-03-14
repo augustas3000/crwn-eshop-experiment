5ad2d94a88f258f6533427d4441db19228d0c2b9
augustas3000 <augusto.pasaulis@gmail.com>
2021-03-14 10:54:14 +0200

    styling for homepage enabled plus media query

--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -1,5 +1,9 @@
 import React from "react";
+<<<<<<< HEAD
 import "./homepage.styles.scss";
+=======
+import './homepage.styles.scss'
+>>>>>>> 70d182c (styling for homepage enabled plus media query)
 import Directory from "../../components/directory/directory.component";
 
 const HomePage = () => {
